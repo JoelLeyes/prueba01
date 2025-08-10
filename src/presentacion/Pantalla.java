b177de79b2e78491b5064f8123b4e48f5f1153ee
joel <joel.leyes@estudiantes.utec.edu.uy>
2025-08-09 21:07:30 -0300

    ALVARO PUTO

--- src/presentacion/Pantalla.java
+++ src/presentacion/Pantalla.java
@@ -9,6 +9,8 @@ import javax.swing.JToolBar;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 
 public class Pantalla extends JFrame {
 
@@ -25,29 +27,41 @@ public class Pantalla extends JFrame {
 	 */
 	public Pantalla() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 497, 317);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		JMenu mnNewMenu = new JMenu("registro");
+		mnNewMenu.setBounds(130, 10, 99, 26);
 		contentPane.add(mnNewMenu);
 		
 		JMenuItem mntmNewMenuItem = new JMenuItem("registrar usuario");
 		mnNewMenu.add(mntmNewMenuItem);
 		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(288, 138, 39, 11);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setBounds(130, 124, 39, 11);
+		contentPane.add(lblNewLabel_2);
+		
 		JToolBar toolBar = new JToolBar();
+		toolBar.setBounds(322, 16, 30, 13);
 		contentPane.add(toolBar);
 		
 		JLabel hola = new JLabel("hola");
 		toolBar.add(hola);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		toolBar.add(lblNewLabel_2);
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(322, 81, 112, 26);
+		contentPane.add(textPane);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		toolBar.add(lblNewLabel_1);
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(237, 42, 90, 42);
+		contentPane.add(textArea);
 
 	}
-
 }
